ca43d05d5cb83818a811a945d1a418018428a5bd
Kuhmat-Tesla <kuhangamateus30@gmail.com>
2026-05-28 00:29:16 +0100

    Complete payments CRUD

--- routes/payments.py
+++ routes/payments.py
@@ -3,22 +3,30 @@ from database import get_bd_connection
 
 payments_bp = Blueprint("payments", __name__, url_prefix="/payments")
 
-@payments_bp.route("/create/<debt_id>")
-def create(debt_id):
+@payments_bp.route("/create/<customer_id>", methods=["POST"])
+def create(customer_id):
     amount = request.form["amount"]
-    note = request.form("amount")
+    note = request.form.get("note")
     if not amount:
-        return jsonify({"success": False, "error": "Campos vazios"}), 200
+        return jsonify({"success": False, "error": "Campos vazios"}), 400
     with get_bd_connection() as conn:
         cursor = conn.cursor()
-        cursor.execute("""INSERT INTO payments (amount, note, debt_id) VALUES (?, ?, ?)""", (amount, note, debt_id)
+        cursor.execute("""INSERT INTO payments (amount, note, customer_id) VALUES (?, ?, ?)""", (amount, note, customer_id)
         )
-    return jsonify({"sucess": True}), 200
+    return jsonify({"success": True}), 200
 
-@payments_bp.route("/<debt_id>")
-def get_payments(debt_id):
-    return "Get payment"
+@payments_bp.route("/<customer_id>", methods=["GET"])
+def get_payments(customer_id):
+    with get_bd_connection() as conn:
+        cursor = conn.cursor()
+        cursor.execute("""SELECT * FROM payments WHERE customer_id = ?""", (customer_id,))
+        rows = cursor.fetchall()
+        data = jsonify([dict(row) for row in rows])
+        return data
 
-@payments_bp.route("/delete/<id>")
+@payments_bp.route("/delete/<id>", methods=["DELETE"])
 def delete_payments(id):
-    return "Delete payment"
+    with get_bd_connection() as conn:
+        cursor = conn.cursor()
+        cursor.execute("""DELETE FROM payments WHERE id = ?""", (id,))
+    return jsonify({"success": True}), 200
